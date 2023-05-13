a04e7bbfd14d8e0c2dcdc46373dd019ec259da99
zktm9903 <zktm9903@naver.com>
2023-05-13 15:02:12 +0900

    자바코드 구성 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,37 +2,44 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-
+@Configuration
 public class HellobootApplication {
-	public static void main(String[] args) {
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-			applicationContext.registerBean(HelloController.class);
-			applicationContext.registerBean(SimpleHelloService.class);
-			applicationContext.refresh();
+	@Bean
+	public HelloController helloController(HelloService helloService){
+		return new HelloController(helloService);
+	}
 
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
+	@Bean
+	public HelloService helloService(){
+		return new SimpleHelloService();
+	}
+	public static void main(String[] args) {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+									new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
+		applicationContext.register(HellobootApplication.class);
+		applicationContext.refresh();
+
+
 	}
 }
