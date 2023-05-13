f7aa2552d874cf59ff3e1f87cb752f9a002e1695
zktm9903 <zktm9903@naver.com>
2023-05-13 15:19:18 +0900

    @Controller @Service

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,6 +1,7 @@
 package tobyspring.helloboot;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RequestMapping("/hello")
-@MyComponent
+@Controller
 public class HelloController {
     private final HelloService helloService;
 
